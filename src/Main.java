f9f10e9c7bc6a01e977a3dae39d9da72dcff539a
crisneacsu <145997878+crisneacsu@users.noreply.github.com>
2023-09-25 16:44:37 +0300

    Al2lea Commit.java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Va rog frumos sa ma lasati sa plec mai rapid sa merg la medic!");
     }
 }
